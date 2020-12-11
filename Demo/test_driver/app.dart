adab40261b7ef6991e560953ec65e7c7cc522e94
rajarahul12 <maddula.rajarahul@gmail.com>
2020-12-11 12:19:05 +0530

    Consolidating changes

--- Demo/test_driver/app.dart
+++ Demo/test_driver/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:Demo/main.dart' as app;
+import "package:Demo/main.dart" as app;
 
 void main() {
   // This line enables the extension.
